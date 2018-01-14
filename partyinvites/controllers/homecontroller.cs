a09bb8d7fa3c61e31e3e4f600d0da97ecf6be0a5
Joshua <shatakan@myway.com>
2018-01-14 19:06:21 +0000

    Deleted something in RSVP

---
+++ partyinvites/controllers/homecontroller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning, " : "Good afternoon, ";
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Store response from guest
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+    }
+}
