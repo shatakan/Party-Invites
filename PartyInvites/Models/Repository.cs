534722e28c1e933c34b10203a09814f824a1ad43
Josh <joshbailey545@gmail.com>
2018-01-15 03:16:43 +0000

    Fixed broken "Thank you" View

--- PartyInvites/Models/Repository.cs
+++ PartyInvites/Models/Repository.cs
@@ -6,7 +6,8 @@ namespace PartyInvites.Models
     {
         private static List<GuestResponse> responses = new List<GuestResponse>();
 
-        public static IEnumerable<GuestResponse> Responses {
+        public static IEnumerable<GuestResponse> Responses
+        {
             get { return responses;}
         }
         public static void AddResponse(GuestResponse response)
